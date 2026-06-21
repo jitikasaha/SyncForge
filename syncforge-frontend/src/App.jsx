d3ae812d4e307a8b046dcdeed5007d42ba9068d9
Jitika Saha <sahajitika@gmail.com>
2026-06-21 18:07:37 +0530

    Create React frontend and landing page

---
+++ syncforge-frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">SyncForge</h1>
+
+      <p className="subtitle">
+        Real-Time Collaborative Development Platform
+      </p>
+
+      <div className="buttons">
+        <button className="btn">Create Room</button>
+
+        <button className="btn">Join Room</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
